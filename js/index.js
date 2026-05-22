fccc05dd87de99b9926fa95cb3bd170df2882961
Irelys <irelyscasillas@gmail.com>
2026-05-21 20:10:37 -0400

    Actualizacion

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 // Footer
-let date = new Date()
-let thisYear = date.getFullYear()
+let today = new Date()
+let thisYear = today.getFullYear()
 
 let footer = document.querySelector('footer')
+document.body.appendChild(footer)
+
 let copyRight = document.createElement('p')
 copyRight.innerHTML = `&copy; ${thisYear} Irelys Casillas`
 footer.appendChild(copyRight)
@@ -10,7 +12,9 @@ footer.appendChild(copyRight)
 
 // Add Skills
 let skills = ['HTML', 'CSS', 'JavaScript', 'Git', 'GitHub', 'Problem Solving', 'Continuous Learning']
-let skillsList = document.querySelector('#skills ul')
+
+let skillsSection = document.getElementById('skills')
+let skillsList = skillsSection.querySelector('ul')
 
 for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement('li')
